51ec500025c063f30799372d49c74857321447a5
cataling6 <radoi.catalin88@gmail.com>
2023-12-19 08:58:33 +0100

    up esercizi

---
+++ ESERCIZI.js
@@ -0,0 +1,55 @@
+// 1) crea una funzione che controlli due numeri interi. Ritorna  true se uno dei due è 50 o se la somma dei 2 è 50
+
+// 2) crea una funzione che rimuova il carattere ad una specifica posizione da una stringa. Passa la stringa e la posizione come
+// parametri e ritorna la stringa modificata
+
+// 3) crea una funzione che controlli se due numeri siano compresi tra 40-60 o tra 70-100. ritorna true se rispecchiano queste condizioni altrimenti false.
+
+// 4) crea una funzione che accetti un nome di città come parametro e ritorni il nome stesso se inizia con "Los" o "New" altrimenti false
+
+// 5) crea una funzione che calcoli e ritorni la somma di tutti gli elementi di un array (che sarà il parametro)
+
+// 6) crea una funzione che controlli he un array NON contenga i numeri 1 o 3. se NON li contiene, ritorna true altirmenti false
+
+// 7) crea una funzione che ritorni il tipo di angolo i cui gradi vengono passati come parametro:
+// meno di 90 -> acuto
+// 90-180 -> ottuso
+// 90 -> retto
+// 180 -> piatto
+
+// 8) crea una funzione che crei acronimo da partire da uan frase. es: Fabbrica Italiana Automobili Torino -> FIAT
+
+//:::::::::::::::::::::: Esercizi extra :::::::::::::::::::::::::::::::
+// NOTA: tutti gli esercizi devono essere svolti usando le funzioni
+// 1. Partendo da una stringa (passata come parametro), ritorna il carattere più usato nella stringa stessa.
+// 2. Controlla che due stringhe passate come parametri siano gli anagrammi l’una dell’altra. Ignora punteggiatura e spazi e ricordate di rendere la stringa tutta in minuscolo. Se le due parole sono anagrammi, ritorna `true`, altrimenti ritorna `false`.
+// 3. Partendo da una lista di possibili anagrammi e da una parola (entrambi passati come parametri), ritorna un nuovo array contenente tutti gli anagrammi corretti della parola data.
+// Per esempio, partendo da “cartine” e [”carenti”, “incerta”, “espatrio”], il valore ritornato deve essere [”carenti”, “incerta”].
+// 4. Partendo da una stringa passata come parametro, ritorna `true` se la stringa è palindroma o `false` se non lo è.
+// 5. Partendo da un numero intero (dai parametri) ritorna un numero che contenga le stesse cifre, ma in ordine contrario. Es. 189 ⇒ 981
+// 6. Scrivi una funzione che accetti un numero positivo X come parametro. La funzione dovrebbe stampare a console una “scala” creata con il carattere “#” e avente X scalini.
+// Es.
+// X = 2
+// '# '
+// '##'
+// X = 3
+// '# '
+// '## '
+// '###'
+// 7. Crea una funzione che, data una stringa come parametro, ritorni la stessa stringa, ma al contrario. Es. “Ciao” ****⇒ “oaiC”
+// 8. Crea una funzione che accetti un array e un numero Y come parametro. Dividi l’array in sotto-array aventi lunghezza Y.
+// Es. array: [1, 2, 3, 4], y: 2 ⇒ [[ 1, 2], [3, 4]]
+// array: [1, 2, 3, 4, 5], y: 4 ⇒ [[ 1, 2, 3, 4], [5]]
+// 9. Scrivi una funzione che accetti un numero positivo X come parametro. La funzione dovrebbe stampare a console una “piramide” create con il carattere “#” e avente X strati.
+// Es.
+// X = 3
+// ' # '
+// ' ### '
+// '#####'
+// 10. Scrivi una funzione che accetti un intero N e ritorni una matrice a spirale NxN:
+// Es. N = 2
+// [[1, 2],[4, 3]]
+// N = 3
+// [[1, 2, 3],[8, 9, 4],[7, 6, 5]]
+// N = 4
+// [[1, 2, 3, 4],[12, 13, 14, 5],[11, 16, 15, 6],[10, 9, 8, 7]]
